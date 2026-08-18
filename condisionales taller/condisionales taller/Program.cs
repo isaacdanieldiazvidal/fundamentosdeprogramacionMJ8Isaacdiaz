ac7a678b89264450c0671c7149cc810c700211ae
isaacdanieldiazvidal <isaacd.diaz@upb.edu.co>
2026-08-18 09:40:43 -0500

    condicionales taller

---
+++ condisionales taller/condisionales taller/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace condisionales_taller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Crear un algoritmo que permita leer tres valores y almacenarlos en las variables numero1,
+            //numero2, numero3 respectivamente. El algoritmo debe comprobar si los números son
+            //diferentes, si es así, imprimir cual es el número mayor, además ordenar y mostrar en
+            //pantalla los números de menor a mayor.
+            
+            int numero1 = 0;
+            int numero2 = 0;
+            int numero3 = 0;
+            Console.WriteLine("ingrese tres valores diferentes");
+            Console.WriteLine("ingrese valor 1");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese valor 2");
+            numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese valor 3");
+            numero3 = int.Parse(Console.ReadLine());
+
+            if (numero1 != numero2 && numero1 != numero3 && numero2 != numero3)
+
+                if (numero1 > numero2 && numero1 > numero3)
+                {
+                    Console.WriteLine("el numero 1 es mayor");
+                }
+                if (numero2 > numero1 && numero2 > numero3)
+                {
+                    Console.WriteLine("el numero 1 es mayor");
+                }
+                if (numero3 > numero1 && numero3 > numero2)
+                {
+                    Console.WriteLine("el numero 1 es mayor");
+                }
+
+            if (numero1 == numero2 || numero1 == numero3 || numero2 == numero3) 
+            {
+                Console.WriteLine("ingrese valores diferentes");
+            }
+
+            if (numero1 > numero2 && numero2 > numero3)
+            {
+                Console.WriteLine(" 3 2 1 ");
+
+                    if (numero1 > numero3 && numero3 > numero2)
+                    {
+                    Console.WriteLine(" 2 3 1 ");
+                    }
+            }
+            if (numero2 > numero1 && numero1 > numero3)
+            {
+                Console.WriteLine(numero3 + " " + numero1 + "" + numero2+ " " );
+
+                if (numero2 > numero3 && numero3 > numero1)
+                {
+                    Console.WriteLine(" 1 3 2 ");
+                }
+            }
+            if (numero3 > numero1 && numero1 > numero2)
+            {
+                Console.WriteLine(" 2 1 3 ");
+
+                if (numero3 > numero2 && numero2 > numero1)
+                {
+                    Console.WriteLine(" 1 2 3 ");
+                }
+            }
+}
---
+++ condisionales taller/condisionales taller/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("condisionales taller")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("condisionales taller")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("957eb6e1-ba1b-4c35-928e-be456c5b2ac5")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
