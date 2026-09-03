fa46b132b8f9a1ae24e40aa04663f45cdb98d5ba
isaacdanieldiazvidal <isaacd.diaz@upb.edu.co>
2026-09-03 09:01:03 -0500

    12 Siclos Do While

---
+++ 12SicloDoWhile/12SicloDoWhile/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12SicloDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string numeroCuenta;
+            double saldo = 0;
+
+            int cantidadUsuarios = 0;
+            double sumaSaldos = 0;
+            double promedio = 0;
+
+            char continuar;
+
+            do
+            {
+                Console.WriteLine("Ingrese el nombre del usuario:");
+                nombre = Console.ReadLine();
+
+                Console.WriteLine("Ingrese el número de cuenta:");
+                numeroCuenta = Console.ReadLine();
+
+                Console.WriteLine("Ingrese el saldo:");
+                saldo = double.Parse(Console.ReadLine());
+
+                cantidadUsuarios++;
+                sumaSaldos = sumaSaldos + saldo;
+
+                if (saldo > 3000000)
+                {
+                    Console.WriteLine("Nombre: " + nombre);
+                    Console.WriteLine("Número de cuenta: " + numeroCuenta);
+                    Console.WriteLine("Saldo: " + saldo);
+                    Console.WriteLine("Es apto para el crédito.");
+                }
+                else
+                {
+                    Console.WriteLine("Nombre: " + nombre);
+                    Console.WriteLine("Número de cuenta: " + numeroCuenta);
+                    Console.WriteLine("Saldo: " + saldo);
+                    Console.WriteLine("No es apto para el crédito.");
+                }
+
+                Console.WriteLine("¿Desea ingresar otro usuario? (S/N)");
+                continuar = char.Parse(Console.ReadLine());
+
+            } while (continuar == 'S' || continuar == 's');
+
+            if (cantidadUsuarios > 0)
+            {
+                promedio = sumaSaldos / cantidadUsuarios;
+
+                Console.WriteLine("Cantidad de usuarios: " + cantidadUsuarios);
+                Console.WriteLine("Promedio de los saldos: " + promedio);
+            }
+            else
+            {
+                Console.WriteLine("No se ingresaron usuarios.");
+            }
+        }
+    }
+}
---
+++ 12SicloDoWhile/12SicloDoWhile/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("12SicloDoWhile")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("12SicloDoWhile")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("451dd6c1-fd36-48a9-b78b-80b7528f2a4c")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
