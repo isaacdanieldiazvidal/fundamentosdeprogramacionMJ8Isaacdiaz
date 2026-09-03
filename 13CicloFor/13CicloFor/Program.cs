1f4c979ad958f45483d5a6998b9c5cf697ca4ca3
isaacdanieldiazvidal <isaacd.diaz@upb.edu.co>
2026-09-03 09:30:26 -0500

    13 Ciclos For

---
+++ 13CicloFor/13CicloFor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _13CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+
+            for (int contador = 1; contador <= 5; contador++)
+            {
+                acumulador += contador;
+            }
+            Console.WriteLine($"Lasuma de los cinco primeros numeros es: " + acumulador);
+        }
+    }
+    }
+}
