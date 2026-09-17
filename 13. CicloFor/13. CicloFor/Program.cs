6d4be84406c6e6b59a96cd81da84eb783d9a0558
isaacdanieldiazvidal <isaacd.diaz@upb.edu.co>
2026-09-16 22:31:17 -0500

    13.5.CicloFor

---
+++ 13. CicloFor/13. CicloFor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _13.CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inicio;
+            int final;
+
+            Console.WriteLine("Ingrese el número inicial del rango:");
+            inicio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número final del rango:");
+            final = int.Parse(Console.ReadLine());
+
+            for (int i = inicio; i <= final; i++)
+            {
+                if (i / 5 * 5 == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
