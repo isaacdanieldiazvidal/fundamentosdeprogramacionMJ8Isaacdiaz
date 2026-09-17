8e0fde47e6df3f6f60e92c357983b9bf9a370826
isaacdanieldiazvidal <isaacd.diaz@upb.edu.co>
2026-09-16 22:19:08 -0500

    10. CiclosWhile3

---
+++ 10. CiclosWhile3/10. CiclosWhile3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _10.CiclosWhile3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+
+            int numeroSecreto = aleatorio.Next(1, 101);
+            int intento = 0;
+
+            while (intento != numeroSecreto)
+            {
+                Console.WriteLine("Adivina el número entre 1 y 100:");
+                intento = int.Parse(Console.ReadLine());
+
+                if (intento > numeroSecreto)
+                {
+                    Console.WriteLine("El número es demasiado alto.");
+                }
+                else
+                {
+                    if (intento < numeroSecreto)
+                    {
+                        Console.WriteLine("El número es demasiado bajo.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("¡Correcto! Adivinaste el número.");
+                    }
+                }
+            }
+        }
+    }
+}
