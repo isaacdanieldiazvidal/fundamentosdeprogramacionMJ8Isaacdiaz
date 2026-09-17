e6326654c62e951ddfb5198681c2bc75eeecd95b
isaacdanieldiazvidal <isaacd.diaz@upb.edu.co>
2026-09-16 21:58:48 -0500

    7.5 .cicloswhile

---
+++ 7.2CicloWhile/7.2CicloWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace _7._2CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int factorial = 1;
+            int contador = 1;
+
+            Console.WriteLine("Ingrese un número:");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                Console.WriteLine("No se puede calcular el factorial de un número negativo.");
+            }
+            else
+            {
+                while (contador <= numero)
+                {
+                    factorial = factorial * contador;
+                    contador++;
+                }
+
+                Console.WriteLine("El factorial de " + numero + " es: " + factorial);
+            }
+        }
+    }
+}
